bd9cd51368379524e80e2b9195670570bd68e168
emmakuen <emmaku@protonmail.com>
2021-12-30 23:41:07 +0900

    display photos

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,13 +1,45 @@
+const imgContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
 // Get photos from Unsplash API
 const getPhotos = async () => {
   try {
     const res = await fetch(apiUrl);
     const data = await res.json();
-    console.log(data);
+    return data;
   } catch (err) {
     // Catch Error
   }
 };
 
+// Create Elements for Links, Photos, Add to DOM
+const displayPhotos = (photos) => {
+  photos.forEach((photo) => {
+    // Create <a> to Unsplash
+    const linkEl = document.createElement("a");
+    setAttributes(linkEl, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // Create <img> for photo
+    const imageEl = document.createElement("img");
+    setAttributes(imageEl, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Put <img> inside <a>, put poth inside imgContainer
+    linkEl.appendChild(imageEl);
+    imgContainer.appendChild(linkEl);
+  });
+};
+
+// Helper to Set Attributes
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
 // On Load
-getPhotos();
+getPhotos().then((photos) => displayPhotos(photos));
